d5b9fbfb9a5dd3a9b81cd8ffc2ba5b23bba25644
Blazhe Manev <blaze_manev@yahoo.com>
2024-11-23 13:02:28 +0100

    Menu bar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 // src/App.js
-
-import React from 'react';
-import Satellites from './components/Satellite/SatellitesComponenet';  // Import Earth Component
+import React, { useState } from 'react';
+import MenuBar from './components/MenuBar';  // Import MenuBar
+import Satellites from './components/Satellite/SatellitesComponenet';  // Import Satellites Component
 
 function App() {
+  const [currentView, setCurrentView] = useState("track"); // Default to "track" view
+
+  // Function to handle navigation and update the view
+  const handleNavigate = (view) => {
+    setCurrentView(view);
+  };
+
   return (
     <div style={{ width: '100%', height: '100%' }}>
-      <Satellites />
+      <MenuBar onNavigate={handleNavigate} /> {/* Render the MenuBar with the navigation handler */}
+
+      {/* Conditionally render the component based on the currentView state */}
+      {currentView === "track" && <Satellites />}
     </div>
   );
 }
